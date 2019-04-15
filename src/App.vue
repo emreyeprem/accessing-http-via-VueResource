329176730b6bd71343b996be49a2883baf793b8f
Emre Akurek <emreyeprem@gmail.com>
2019-04-15 15:47:26 -0500

    design in progress

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Accessing Http</h1>
+                <div class="form-group">
+                  <label>Username</label>
+                  <input class="form-control" type="text">
+                </div>
+                <div class="form-group">
+                  <label>Mail</label>
+                  <input class="form-control" type="text">
+                </div>
+                <button class="btn btn-primary">Submit</button>
             </div>
         </div>
     </div>
